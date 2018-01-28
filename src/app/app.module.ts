8e93a682843a72d3b440b7788394ad638fea728b
Adir Elfersy <adir.elfersy@gmail.com>
2018-01-28 11:53:06 +0200

    init routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
+import { GoodByeComponent } from './good-bye/good-bye.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { Route } from '@angular/compiler/src/core';
+import { HelloComponent } from './hello/hello.component';
 
+const routes: Routes = [
+  { path: 'hello', component: HelloComponent },
+  { path: 'goodbye', component: GoodByeComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoodByeComponent,
+    HelloComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
